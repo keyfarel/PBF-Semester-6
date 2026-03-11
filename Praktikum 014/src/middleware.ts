1591b2e47795ad09afb1397c52874653330a5fe8
keyfarel <carinsmddd@gmail.com>
2026-03-11 08:00:00 +0700

    feat(praktikum-014): initial commit

---
+++ Praktikum 014/src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const isLogin = false;
+
+  if (!isLogin) {
+    return NextResponse.redirect(new URL('/auth/login', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/produk', '/about'],
+}
